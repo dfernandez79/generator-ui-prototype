75362714892488ca53236057b78ad9fa236e0a39
Diego Fernandez <diego79@gmail.com>
2014-04-19 02:08:08 -0300

    Updated example

--- app/templates/src/scripts/livereload-support.js
+++ app/templates/src/scripts/livereload-support.js
@@ -1,12 +1,12 @@
 (function () {
-	/* global document, window */
-	'use strict';
+  /* global document, window */
+  'use strict';
 
   var
     hostname = (window.location.host || 'localhost').split(':')[0],
     livereloadPort = 35730;
 
-  if (hostname === 'localhost' || hostname === '127.0.0.1') {
+  if (hostname === 'localhost' || hostname === '127.0.0.1' || hostname === '0.0.0.0') {
     document.write('<script src="http://' + hostname + ':' + livereloadPort + '/livereload.js"></' + 'script>');
   }
 
